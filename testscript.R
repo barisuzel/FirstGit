e5131ff5c5fda2951f1e5d64d4f1bdb7d9bd7cf9
barisuzel <uzelba@gmail.com>
2020-01-22 14:40:26 -0500

    Second commit

--- testscript.R
+++ testscript.R
@@ -1,1 +1,7 @@
 #First Sinked Script
+#Once you click commit your code is the new official version - It is still local
+#Click the push button to upload to github
+#Pull --> take something on github to your own computer. Perhaps a collaborators
+10 + 2
+
+x<- 22
